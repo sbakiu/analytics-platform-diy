58e4c93d28c02f45ecc6f82dee370907786b16e3
Sadik Bakiu <sbakiu@gmail.com>
2021-03-21 17:15:47 +0100

    Add missing link

--- minikube/macos_docker_push_minikube_registry.sh
+++ minikube/macos_docker_push_minikube_registry.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#This is needed to allow docker to push images to minikube's registry
+#This is needed to allow docker to push images to minikube's registry: https://minikube.sigs.k8s.io/docs/handbook/registry/#docker-on-macos
 minikube addons enable registry
 docker run --rm -it --network=host alpine ash -c "apk add socat && socat TCP-LISTEN:5000,reuseaddr,fork TCP:$(minikube ip):5000"
